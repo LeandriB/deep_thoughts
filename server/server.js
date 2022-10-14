5eafb22a4acc7d8cb8feb20aa506ae8f83ad9af4
Leandri Beavis <leandrikuyk@gmail.com>
2022-10-13 20:31:45 -0400

    added authMiddleware method to utils and added to context method in ApolloServer

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -6,12 +7,14 @@ const { ApolloServer } = require('apollo-server-express');
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 //create a new Apollo server and pass in our Schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers, 
+  context: authMiddleware
 });
 
 const app = express();
