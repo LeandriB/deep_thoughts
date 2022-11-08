f600337469507de088dd76b1320e52199781f266
Leandri Beavis <leandrikuyk@gmail.com>
2022-11-08 09:10:52 -0500

    Intergrate Apollo Hooks and set up queries

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const { ApolloServer } = require('apollo-server-express');
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 //create a new Apollo server and pass in our Schema data
