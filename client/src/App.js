aa79478c8459e108ff3d0728675a525bf69f6bf7
Leandri Beavis <leandrikuyk@gmail.com>
2022-11-29 16:33:09 -0500

    set up main URL routes using react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Home from './pages/Home';
 
@@ -20,13 +26,40 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-        <div className='flex-column justify-flex-start min-100-vh'>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
           <Header />
-            <div className='container'>
-              <Home />
-            </div>
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
+              <Route
+                path="/thought"
+                element={<SingleThought />}
+              />
+              <Route
+                path="/nomatch"
+                element={<NoMatch/>}
+              />
+            </Routes>
+          </div>
           <Footer />
-      </div>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
